204061df564b120e437384dc7d5225cb3d5dabcc
Joseph DeWeese <jdeweese@weldtascics.systems>
2024-02-22 10:57:14 -0600

    Initial Commit

---
+++ PhotoPickerSample/PhotoPickerSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPickerSampleApp.swift
+//  PhotoPickerSample
+//
+//  Created by J. DeWeese on 2/22/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoPickerSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
